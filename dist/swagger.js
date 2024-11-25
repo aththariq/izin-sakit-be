1f5e557ae882a2203e6d267fbd6c4a5f4efab0a8
Aththariq Lisan Qur'an Daulah Sentono <130519479+aththariq@users.noreply.github.com>
2024-11-25 08:08:35 +0700

    .

--- dist/swagger.js
+++ dist/swagger.js
@@ -1,28 +1,30 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const swagger_jsdoc_1 = __importDefault(require("swagger-jsdoc"));
 const swaggerDefinition = {
-    openapi: "3.0.0",
-    info: {
-        title: "Laundry App API",
-        version: "1.0.0",
-        description: "API documentation for the Laundry App Backend",
+  openapi: "3.0.0",
+  info: {
+    title: "Laundry App API",
+    version: "1.0.0",
+    description: "API documentation for the Laundry App Backend",
+  },
+  servers: [
+    {
+      url: "http://localhost:5001",
     },
-    servers: [
-        {
-            url: "http://localhost:5001",
-        },
-        {
-            url: "https://your-backend.up.railway.app",
-        },
-    ],
+    {
+      url: "web-tst-backend.up.railway.app",
+    },
+  ],
 };
 const options = {
-    swaggerDefinition,
-    apis: ["./src/routes/*.ts"],
+  swaggerDefinition,
+  apis: ["./src/routes/*.ts"],
 };
 const swaggerSpec = (0, swagger_jsdoc_1.default)(options);
 exports.default = swaggerSpec;
